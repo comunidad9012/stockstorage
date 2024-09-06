9af82f02359681a49fcd20d2ec199951f59e1f8a
gpanelli3 <gpanelli3@gmail.com>
2024-09-06 10:41:03 -0300

    configuracionDocker

--- backend/modelsDb/conexion.py
+++ backend/modelsDb/conexion.py
@@ -4,11 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 #conexion con SQLALCHEMY
 host="localhost"
-user="genarodesarrollo"
+user="usuario"
 password="password"
-db="sistema"
+db="stockstorage"
 
-engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}:{3306}/{db}')
+engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}:{3307}/{db}')
 Session=sessionmaker(bind=engine)
 session=Session()
 Base=declarative_base()
