6baddea7d82055f26856cb9b0a76c8f6e5d3a7c4
Mauro Andrade <mauritomza@hotmail.com>
2024-09-17 21:36:52 -0300

    new:user_schema creado

--- backend/controllers/test.py
+++ backend/controllers/test.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
 from modelsDb.model_usuario import Usuario
+from schemas.user_schema import userRegisterSchema
+from marshmallow import ValidationError
 
 from modelsDb import conexion
 
@@ -7,12 +9,20 @@ apiUser= Blueprint('usertest', __name__, url_prefix='/usertest')
 
 @apiUser.post('/prueba')
 def testUser():
-    email=request.json['email']
-    password=request.json['password']
-    new_user=Usuario(email,password)
-    conexion.session.add(new_user)
-    conexion.session.commit()
-    return (f'{email} cargado')
+    try:
+        email = request.json['email']
+        password = request.json['password']
+        newUser = {
+            "email": email,
+            "password": password
+        }
+        userValidation= userRegisterSchema().load(newUser)
+        userValidado= Usuario(email=userValidation['email'], password=userValidation['password'])
+        conexion.session.add(userValidado)
+        conexion.session.commit()
+        return (f'{email} cargado')
 
+    except ValidationError as badValidation:
+        print(f'error {badValidation}')
 
 
